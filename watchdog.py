2f7b2bcb7c315a6c58df45b9fe2648ca54b758d5
Jakub Slawinsky <jakubslawinsky@gmail.com>
2024-02-18 18:33:44 +0100

    create final v1 version of watchdog.py

--- watchdog.py
+++ watchdog.py
@@ -1,23 +1,20 @@
 import re
 import time
 
-FILE_PATH = "/var/log/traffic.log"
-def find_ip_addresses(FILE_PATH):
+def find_ip_addresses(line):
     ip_regex = r'\b(?:\d{1,3}\.){3}\d{1,3}\b'
-    with open(FILE_PATH, 'r') as file:
-        for line in file:
-            ip_addresses = re.findall(ip_regex, line)
-            for ip in ip_addresses:
-                print("Found IP address:", ip)
+    ip_addresses = re.findall(ip_regex, line)
+    for ip in ip_addresses:
+        print("Found IP address:", ip)
 
-def monitor_file(FILE_PATH):
+
+def monitor_file(path):
+    file = open(path)
+    file.seek(0, 2)
     while True:
-        with open(FILE_PATH, 'r') as file:
-            file.seek(0, 2)
-            while True:
-                line = file.readline()
-                if not line:
-                    break
-                print("New line detected:", line.strip())
-                find_ip_addresses(FILE_PATH)
-        time.sleep(1)
+        line = file.readline()
+        if not line:
+            time.sleep(0.1)
+            continue
+        print("New line detected:", line.strip())
+        find_ip_addresses(line.strip())
