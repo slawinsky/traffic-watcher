9f8baca5786b8a444d1857acc125ae7e23ddfff1
Jakub Slawinsky <jakubslawinsky@gmail.com>
2024-03-03 20:50:45 +0100

    add blocking ip addresses via ebtables

---
+++ ebtables.py
@@ -0,0 +1,6 @@
+import os
+
+
+def block_ip_address(ip):
+    os.system(f'sudo ebtables -A FORWARD -p ivp4 -i wlan0 --ip-src {ip} -j DROP')
+    os.system(f'sudo ebtables -A INPUT -p ivp4 -i wlan0 --ip-dst {ip} -j DROP')
