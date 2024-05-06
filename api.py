0ff4142dd0ebba8ecf257ec630a6949d12a89da2
Jakub Slawinsky <jakubslawinsky@gmail.com>
2024-05-06 22:40:50 +0200

    surround api request with try

--- api.py
+++ api.py
@@ -4,9 +4,12 @@ URL = 'https://api.ipapi.is/'
 
 
 def send_request(endpoint):
-    url = URL + endpoint
-    response = requests.get(url)
-    return response
+    try:
+        url = URL + endpoint
+        response = requests.get(url)
+        return response
+    except Exception:
+        Exception('Could not send request to API')
 
 
 def get_ip_address_report(ip):
