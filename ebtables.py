5332e7e33f914f398d173b94926307f6e017388d
Jakub Slawinsky <jakubslawinsky@gmail.com>
2024-05-06 22:36:36 +0200

    surround ebtables rule adding with try

--- ebtables.py
+++ ebtables.py
@@ -2,5 +2,8 @@ import os
 
 
 def block_ip_address(ip):
-    os.system(f'sudo ebtables -A FORWARD -p ivp4 -i wlan0 --ip-src {ip} -j DROP')
-    os.system(f'sudo ebtables -A INPUT -p ivp4 -i wlan0 --ip-dst {ip} -j DROP')
+    try:
+        os.system(f'sudo ebtables -A FORWARD -p ivp4 -i wlan0 --ip-src {ip} -j DROP')
+        os.system(f'sudo ebtables -A INPUT -p ivp4 -i wlan0 --ip-dst {ip} -j DROP')
+    except Exception:
+        print('Could not add ebtables rule')
