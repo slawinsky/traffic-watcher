dd7d5948cf668d24f02530d16671cb73f9eb888a
Jakub Slawinsky <jakubslawinsky@gmail.com>
2024-05-06 22:30:48 +0200

    add exceptions to file exceptions.py

--- collector.py
+++ collector.py
@@ -1,10 +1,11 @@
 from db import check_if_ip_exists, add_to_db
 from analyzer import analyze_ip
 from ebtables import block_ip_address
+from exceptions import exceptions
 
 
 def collect_ip(ip):
-    if not check_if_ip_exists(ip):
+    if not check_if_ip_exists(ip) and ip not in exceptions:
         print('doesnt exists')
         isDanger = analyze_ip(ip)
         if isDanger:
